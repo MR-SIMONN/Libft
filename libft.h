a84b1a5b673019d6e9dae8bd43f175d9e11ecc75
Mohamed El haia <moel-hai@e3r9p10.1337.ma>
2024-10-22 22:25:05 +0100

    up

--- libft.h
+++ libft.h
@@ -13,13 +13,15 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlen(const char *s );
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
